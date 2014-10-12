bb6a544543e8e11a92ccc2f31c4a1e672efbdfc7
hoblin <evgeny.gurin@gmail.com>
2014-10-13 00:51:08 +0300

    Fix default scale selection for multiple forms on page

--- lib/formtastic/inputs/wysihtml5_input.rb
+++ lib/formtastic/inputs/wysihtml5_input.rb
@@ -237,10 +237,12 @@ module Formtastic
                 <div class="input radio">
                   <div class="asset_scale_selection">
                     <label>#{I18n.t("wysihtml5.dialog.image.scale")}</label>
-                    <label class="option"><input value="full" type="radio" name="scale" checked="checked" /> 100%</label>
-                    <label class="option"><input value="three_quarters" type="radio" name="scale" /> 75%</label>
-                    <label class="option"><input value="half" type="radio" name="scale" /> 50%</label>
-                    <label class="option"><input value="one_quarter" type="radio" name="scale" /> 25%</label>
+                    <form>
+                      <label class="option"><input value="full" type="radio" name="scale" checked="checked" /> 100%</label>
+                      <label class="option"><input value="three_quarters" type="radio" name="scale" /> 75%</label>
+                      <label class="option"><input value="half" type="radio" name="scale" /> 50%</label>
+                      <label class="option"><input value="one_quarter" type="radio" name="scale" /> 25%</label>
+                    </form>
                   </div>
                 </div>
                 <div class="input select">
